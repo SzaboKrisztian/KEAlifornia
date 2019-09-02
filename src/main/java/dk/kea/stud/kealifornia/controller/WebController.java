5a6e6d2e3c68316bf8adfc882e8721b5705b2727
OnescuRadu <onescuradu@yahoo.com>
2019-09-02 23:58:36 +0200

    Login System Implemented

---
+++ src/main/java/dk/kea/stud/kealifornia/controller/WebController.java
@@ -0,0 +1,28 @@
+package dk.kea.stud.kealifornia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/index")
+    public String showIndex() {
+        return "/index";
+    }
+
+    @GetMapping("/")
+    public String showIndex2() {
+        return "/index";
+    }
+
+    @GetMapping("/admin/index")
+    public String showIndex3() {
+        return "/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+}
