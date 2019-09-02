a75f3b7e5e4f30e8e5ffe675bab06afbd5dc4d8d
Radu <onescuradu@yahoo.com>
2019-09-02 16:11:45 +0200

    Security Config

---
+++ src/main/java/dk/kea/stud/kealifornia/configurations/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package dk.kea.stud.kealifornia.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * The authentication configuration that selects the users and their roles from the database
+     * Default format for role is 'ROLE_' + 'YOURROLE'
+     */
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password, enabled from user where username=?")
+                .authoritiesByUsernameQuery("select username, role from user where username = ?");
+
+    }
+
+
+    /**
+     * HTTP Security Configuration that selects which user roles can see which pages
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //enables post mapping
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+                .permitAll();
+        http.exceptionHandling().accessDeniedPage("/403");
+    }
+
+
+}
