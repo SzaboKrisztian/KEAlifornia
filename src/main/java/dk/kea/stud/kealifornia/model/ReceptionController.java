1d6bb6f389bf5fad5dbfa436d00eef6cc8a5da13
Marian Mocanu <gheo0174@stud.kea.dk>
2019-09-03 02:30:25 +0200

    TODO checkIn method

--- src/main/java/dk/kea/stud/kealifornia/model/ReceptionController.java
+++ src/main/java/dk/kea/stud/kealifornia/model/ReceptionController.java
@@ -1,6 +1,7 @@
-package dk.kea.stud.kealifornia;
+package dk.kea.stud.kealifornia.model;
 
 import dk.kea.stud.kealifornia.repository.BookingRepository;
+import dk.kea.stud.kealifornia.repository.OccupancyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ReceptionController {
   @Autowired
   private BookingRepository bookingRepo;
+  @Autowired
+  private OccupancyRepository occupancyRepo;
 
   @GetMapping("/arrival")
   public String arrival() {
@@ -22,17 +25,17 @@ public class ReceptionController {
   @PostMapping("/findBooking")
   public String findBooking(@RequestParam String bookingNo, Model model) {
     model.addAttribute(bookingRepo.findBookingByRefNo(bookingNo));
-
-    //TODO model.addAttribute( list of available rooms )
+    model.addAttribute("checkInForm", new Occupancy());
+    //TODO model.addAttribute( list of available rooms for each category )
 
     return "templates/reception/check-in";
   }
 
   //TODO
   @PostMapping("/checkIn")
-  public String checkIn(@ModelAttribute CheckInForm checkInForm) {
-
+  public String checkIn(@ModelAttribute Occupancy checkInForm) {
+    occupancyRepo.addOccupancy(checkInForm);
+    return "/arrival";
   }
-
 
 }
