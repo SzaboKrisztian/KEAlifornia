393ad933e60b216af4afe927fbb3d3bf1ffeb0d0
OnescuRadu <onescuradu@yahoo.com>
2019-09-05 21:03:51 +0200

    Check-out Done

--- src/main/java/dk/kea/stud/kealifornia/repository/OccupancyRepository.java
+++ src/main/java/dk/kea/stud/kealifornia/repository/OccupancyRepository.java
@@ -89,7 +89,7 @@ public class OccupancyRepository {
     return occupancy;
   }
 
-  public void deleteOccupancy(Occupancy occupancy) {
-    jdbc.update("DELETE FROM occupancies WHERE id = ?;");
+  public void deleteOccupancy(int id) {
+    jdbc.update("DELETE FROM occupancies WHERE id = ?;", id);
   }
 }
